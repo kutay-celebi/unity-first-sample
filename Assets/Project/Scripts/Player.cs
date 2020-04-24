dd4f5fcc3fa8351d9ed72c9b740d509ef7f28176
kutay-celebi <celebikutay@gmail.com>
2020-04-24 19:44:06 +0300

    Init commit.

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [Header("Visuals")] public GameObject visuals;
+    public float rotatingSpeed = 2f;
+
+    [Header("Equipment")] public Sword sword;
+
+    [Header("Movement")] public float jumpingVelocity = 5f;
+    public float movingVelocity = 5f;
+    public float jumpingForce = 100f;
+    private bool canJump = true;
+    public float runSpeed = 3f;
+
+    private Rigidbody playerRigidbody;
+    public Quaternion targetModelRotation;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerRigidbody     = GetComponent<Rigidbody>();
+        targetModelRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // raycast to identify if the player can jump.
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.01f)) {
+            canJump = true;
+        }
+
+        visuals.transform.rotation = Quaternion.Lerp(visuals.transform.rotation, targetModelRotation, rotatingSpeed * Time.deltaTime);
+
+        ProcessInput();
+    }
+
+    void ProcessInput() {
+        playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
+
+        playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            playerRigidbody.velocity = new Vector3(
+                playerRigidbody.velocity.x,
+                playerRigidbody.velocity.y,
+                movingVelocity
+            );
+            // visuals.transform.localEulerAngles = new Vector3(0,180,0);
+            // visuals.transform.rotation = Quaternion.Lerp(visuals.transform.rotation, Quaternion.Euler(0,180,0), rotatingSpeed * Time.deltaTime );
+            targetModelRotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            playerRigidbody.velocity = new Vector3(
+                playerRigidbody.velocity.x,
+                playerRigidbody.velocity.y,
+                -movingVelocity
+            );
+            // visuals.transform.localEulerAngles = new Vector3(0,0,0);
+            // visuals.transform.rotation = Quaternion.Lerp(visuals.transform.rotation, Quaternion.Euler(0,0,0), rotatingSpeed * Time.deltaTime );
+            targetModelRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            playerRigidbody.velocity = new Vector3(
+                -movingVelocity,
+                playerRigidbody.velocity.y,
+                playerRigidbody.velocity.z
+            );
+            // visuals.transform.localEulerAngles = new Vector3(0,90,0);
+            // visuals.transform.rotation = Quaternion.Lerp(visuals.transform.rotation, Quaternion.Euler(0,90,0), rotatingSpeed * Time.deltaTime );
+            targetModelRotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            playerRigidbody.velocity = new Vector3(
+                movingVelocity,
+                playerRigidbody.velocity.y,
+                playerRigidbody.velocity.z
+            );
+            // visuals.transform.localEulerAngles = new Vector3(0,270,0);
+            // visuals.transform.rotation = Quaternion.Lerp(visuals.transform.rotation, Quaternion.Euler(0,270,0), rotatingSpeed * Time.deltaTime );
+            targetModelRotation = Quaternion.Euler(0, 270, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump) {
+            canJump = false;
+            // rigidbody.AddForce(0, jumpingForce, 0);
+            playerRigidbody.velocity = new Vector3(
+                playerRigidbody.velocity.x,
+                jumpingVelocity,
+                playerRigidbody.velocity.z
+            );
+        }
+
+        // check requipment interraction
+        if (Input.GetMouseButtonDown(0)) {
+            sword.Attack();
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Sword.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+    public float swingingSpeed = 2f;
+    public float cooldownSpeed = 2f;
+    public float cooldownDuration = 0.5f;
+    public float attackDuration = 0.35f;
+
+    private bool isAttacking;
+
+    private Quaternion targetRotation;
+    private float cooldownTimer;
+
+    // Start is called before the first frame update
+    void Start() {
+        targetRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation,
+            Time.deltaTime * (isAttacking ? swingingSpeed : cooldownSpeed));
+
+        if (cooldownTimer > 0) {
+            cooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    public void Attack() {
+        if (cooldownTimer > 0f) {
+            return;
+        }
+
+        targetRotation = Quaternion.Euler(-90, 0, 0);
+
+        cooldownTimer = cooldownDuration;
+
+        StartCoroutine(CooldownWait());
+    }
+
+    private IEnumerator CooldownWait() {
+        isAttacking = true;
+        yield return new WaitForSeconds(attackDuration);
+        isAttacking    = false;
+        targetRotation = Quaternion.Euler(0, 0, 0);
+    }
+}
