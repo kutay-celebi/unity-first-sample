455bd30f166ebb6067e69e70387046983a64ce8f
kutay-celebi <celebikutay@gmail.com>
2020-04-26 23:33:22 +0300

    - added camera flow.

---
+++ Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class GameCamera : MonoBehaviour {
+
+    public GameObject target;
+    public Vector3 offset;
+    public float focusSpeed = 1f;
+    
+    // Start is called before the first frame update
+    void Start() {
+        transform.position = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * focusSpeed);
+    }
+}
--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -21,7 +21,7 @@ public class Player : MonoBehaviour {
 
     private Rigidbody playerRigidbody;
     public Quaternion targetModelRotation;
-    private float knockBackSpeed = 50f;
+    public float knockBackSpeed = 100f;
     private float knockBackTimer;
 
     // Start is called before the first frame update
